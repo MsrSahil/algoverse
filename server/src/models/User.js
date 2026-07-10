71acedd78398193e14b8908c023c2d67de74d76c
MsrSahil <mohammadswahil021@gmail.com>
2026-07-10 23:09:20 +0530

    fix: simplify password hashing logic in user schema pre-save hook

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -80,20 +80,15 @@ const userSchema = new mongoose.Schema(
 )
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next()
+    return;
   }
 
-  try {
-    if (this.password) {
+  if (this.password) {
       const hashedPassword = await bcrypt.hash(this.password, 10)
       this.password = hashedPassword
     }
-    next()
-  } catch (error) {
-    next(error)
-  }
 })
 
 // Method to compare passwords
